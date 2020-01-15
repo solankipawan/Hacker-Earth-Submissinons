a80d1362253a1409cc4be73b41b01924a3dc573d
P_Solanki29 <pksaug2529@gmail.com>
2020-01-16 05:21:46 +0530

    Polygon Possibillity

---
+++ Data Structure/Polygon Possibility.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Polygon {
+     static boolean check(int arr[], int n) {
+        int sum = 0, maxS = 0; 
+		for (int i = 0; i < n; i++) { 
+			sum += arr[i]; 
+			maxS = Math.max(arr[i], maxS); 
+		} 
+        if ((sum - maxS) > maxS) 
+			return true; 
+
+		return false; 
+	} 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while(t>0) {
+            int size;
+            size = sc.nextInt();
+            int arr[] = new int[size];
+            for(int i = 0; i<size;i++) {
+                arr[i] =sc.nextInt();
+
+            }
+            int n = arr.length;
+            // check(arr, n);
+            if (check(arr, n)) 
+			System.out.println("Yes"); 
+		else
+			System.out.println("No"); 
+            t--;
+        }
+    }
+}
